c4a1afd609faeab05d468ba12ec56db9bb1dca29
cotttpan <cotttpan@gmail.com>
2017-07-10 08:41:59 +0900

    fix(.gitignore)

--- dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type Path = string | (string | number)[];
-export declare function isPlainObject(obj: object): boolean;
-export declare function omit(src: any, key: string | number): any;
-export declare function rec(src: any, path: Path, value: any, idx: number): any;
-export declare function parsePath(path: Path): (string | number)[];
-export declare function existy(v: any): boolean;
